fed9f4edceb9bdd4b70895bf86357f6bb3a1f137
Rutger de Groen <rutger.degroen@ivengi.com>
2021-11-22 12:56:23 +0100

    case 1 definitief

--- app.ts
+++ app.ts
@@ -2,19 +2,15 @@ import express from "express";
 
 const app = express()
 
-
+// Opstarten server
 app.listen({port: 4000}, ()=> {
     console.log('server running')
 })
 
+// Main route wijst naar de home page
 app.get('/', (_req,res) =>{
     res.sendFile(__dirname+"/index.html");
 })
 
-// const GetAPI = async(url: string) => {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     console.log(data)
-// }
 
 
--- script.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-
-async function GetAPI(){
-    const response = await fetch("https://data.buienradar.nl/2.0/feed/json", {mode: "no-cors"});
-    const data = await response.json();
-    console.log(data)
-}
-
-document.getElementById("zoekButton").addEventListener("click", GetAPI);
